1702dfab840870025899e6875c3fb30a1f6d112e
Anderson Vera <70354869+JhonveraDev@users.noreply.github.com>
2023-11-29 23:09:42 -0500

    time and styles adjusted

--- src/app/addevsoft/graph/graph.component.ts
+++ src/app/addevsoft/graph/graph.component.ts
@@ -9,18 +9,22 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./graph.component.scss']
 })
 export class GraphComponent {
-  dataPoints:any = [];
-  chart:any;
+  dataPoints: any = [];
+  chart: any;
   minValue: any;
   maxValue: any;
   profits: number = 0;
+  months: number = 0;
+  monthsPassed: number = 0;
+  daysPassed: number = 0;
+  yearsPassed: number = 0;
 
   chartOptions = {
     theme: "light2",
     zoomEnabled: true,
     exportEnabled: true,
     title: {
-      text:"Bitcoin Closing Price"
+      text: "Bitcoin Closing Price"
     },
     subtitles: [{
       text: "Loading Data...",
@@ -42,19 +46,24 @@ export class GraphComponent {
     }]
   }
 
-  constructor(private http : HttpClient) {
+  constructor(private http: HttpClient) {
   }
 
   ngAfterViewInit() {
     this.http.get('https://canvasjs.com/data/gallery/angular/btcusd2021.json', { responseType: 'json' }).subscribe((response: any) => {
       let data = response;
-      for(let i = 0; i < data.length; i++){
-        this.dataPoints.push({x: new Date(data[i].date), y: Number(data[i].close) });
+      for (let i = 0; i < data.length; i++) {
+        this.dataPoints.push({ x: new Date(data[i].date), y: Number(data[i].close) });
       }
       this.chart.subtitles[0].remove();
       this.minValue = this.dataPoints[0];
       this.maxValue = this.dataPoints[this.dataPoints.length - 1];
       this.profits = Number(this.maxValue.y - this.minValue.y);
+      this.months = Number(this.maxValue.x - this.minValue.x);
+      this.daysPassed = Math.floor(this.months / (1000 * 60 * 60 * 24));
+      this.monthsPassed = Math.floor(this.daysPassed / 30);
+      this.yearsPassed = Math.floor(this.monthsPassed / 12);
+
     });
   }
 
